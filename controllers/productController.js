87fd02e6448c968bc411eb7a132cfd0a76c54ccb
canon20paul <canon20paul@gmail.com>
2022-11-17 10:27:03 +0300

    First Commit

---
+++ controllers/productController.js
@@ -0,0 +1,18 @@
+const Product = require('../models/Product');
+const { StatusCodes } = require('http-status-codes');
+
+const createProduct = async (req, res) => {
+    // const product = await Product.create(req.body);
+    // res.status(StatusCodes.CREATED).json({ product });
+    res.send('create Product');
+};
+const getAllProducts = async (req, res) => {
+    // const products = await Product.find({});
+    // res.status(StatusCodes.OK).json({ products });
+    res.send('List of products');
+};
+
+module.exports = {
+    createProduct,
+    getAllProducts,
+};
